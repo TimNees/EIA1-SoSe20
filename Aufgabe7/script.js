ea8888fe71063abc76aa00b68d269654f747a7cb
TimNees <64199732+TimNees@users.noreply.github.com>
2020-06-13 19:34:44 +0200

    Aufgabe7

---
+++ Aufgabe7/script.js
@@ -0,0 +1,28 @@
+"use strict";
+//Arry erstellen und jeden Sound einbinden
+var fullSounds = ["A.mp3", "C.mp3", "F.mp3", "G.mp3", "hihat.mp3", "kick.mp3", "laugh-1.mp3", "laugh-2.mp3", "snare.mp3"];
+//querySelector
+document.querySelector("#button1").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button2").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button3").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button4").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button5").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button6").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button7").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button8").addEventListener("click", function () { playsample(0); });
+;
+document.querySelector("#button9").addEventListener("click", function () { playsample(0); });
+;
+// Funktion
+function playsample(buttonnumber) {
+    var sound = new Audio(fullsounds[buttonnumber]);
+    sound.play();
+}
+//# sourceMappingURL=script.js.map
