e7d531c176d855467f09957e85b339888ca228f5
TimNees <64199732+TimNees@users.noreply.github.com>
2020-06-28 19:39:34 +0200

    Update script.js

--- Aufgabe9/script.js
+++ Aufgabe9/script.js
@@ -6,20 +6,29 @@ function drawList() {
     document.querySelector("#todos").innerHTML = "";
     for (let index = 0; index < myList.length; index++) {
         document.querySelector("#todos").innerHTML += "<div>" + "<input type='checkbox'>" + myList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "<div>";
+        var close = document.getElementsByClassName("fas fa-trash-alt");
+        var i;
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function () {
+                var div = this.parentElement;
+                div.style.display = "none";
+            };
+            var total = document.querySelector("#total");
+            total.innerHTML = myList.length;
+        }
     }
-    var total = document.querySelector("#total");
-    total.innerHTML = myList.length;
-}
-document.querySelector("#newTodoButton").addEventListener("click", addNewElement);
-function addNewElement() {
-    var neuerTodoTitel = document.querySelector("#eingabeFeld").value;
-    myList.push(neuerTodoTitel);
+    document.querySelector("#newTodoButton").addEventListener("click", addNewElement);
+    function addNewElement() {
+        var neuerTodoTitel = document.querySelector("#eingabeFeld").value;
+        myList.push(neuerTodoTitel);
+        drawList();
+    }
+    document.querySelector("#trash").addEventListener("click", eintragloeschen);
+    function eintragLoeschen() {
+        clearInterval(index);
+    }
     drawList();
+    //Hilfestellung durch Herr Rausch in der Fragestunde
 }
-document.querySelector("#trash").addEventListener("click", eintragloeschen);
-function eintragLoeschen() {
-    clearInterval(index);
-}
-drawList();
 //Hilfestellung durch Herr Rausch in der Fragestunde
 //# sourceMappingURL=script.js.map
