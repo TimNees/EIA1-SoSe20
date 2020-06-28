6eddb7d32eab0b121d042ad7f31e5cefb7a540db
TimNees <64199732+TimNees@users.noreply.github.com>
2020-06-28 14:53:05 +0200

    Update script.js

--- Aufgabe9/script.js
+++ Aufgabe9/script.js
@@ -1,10 +1,11 @@
 "use strict";
 let myList = ["Hausaufgaben", "Video schneiden", "Lernen"];
+let trash = document.querySelector(".fas fa-trash-alt");
 drawList();
 function drawList() {
     document.querySelector("#todos").innerHTML = "";
     for (let index = 0; index < myList.length; index++) {
-        document.querySelector("#todos").innerHTML += "<div>" + myList[index] + "<div>";
+        document.querySelector("#todos").innerHTML += "<div>" + "<input type='checkbox'>" + myList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "<div>";
     }
 }
 document.querySelector("#newTodoButton").addEventListener("click", addNewElement);
