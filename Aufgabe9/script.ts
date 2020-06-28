3e7400257ec5cccd4325cfaef63dc557f8f76d18
TimNees <64199732+TimNees@users.noreply.github.com>
2020-06-28 17:01:03 +0200

    Update script.ts

--- Aufgabe9/script.ts
+++ Aufgabe9/script.ts
@@ -3,12 +3,16 @@ let myList: string[] = ["Hausaufgaben", "Video schneiden", "Lernen"];
 let trash: HTMLElement = document.querySelector(".fas fa-trash-alt");
 drawList();
 function drawList() {
-    document.querySelector("#todos").innerHTML= "";
+    document.querySelector("#todos").innerHTML = "";
     for (let index = 0; index < myList.length; index++) {
         document.querySelector("#todos").innerHTML += "<div>" + "<input type='checkbox'>" + myList[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "<div>";
+    }
+    var total: HTMLElement = document.querySelector("#total");
+        total.innerHTML = myList.length;
+    }
+
 }
-}
-}
+
 
 document.querySelector("#newTodoButton").addEventListener("click", addNewElement);
 
@@ -18,7 +22,7 @@ function addNewElement() {
     drawList();
 }
 
-document.querySelector("#trash").addEventListener("click", eintragloeschen )
+document.querySelector("#trash").addEventListener("click", eintragloeschen);
 
 function eintragLoeschen() {
     clearInterval(index);
